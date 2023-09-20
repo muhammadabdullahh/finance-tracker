ae1a4fb067715b1f8ed0e22ad4f2b18d70196dfa
theofficialmo <info.abdullahmuhammad@gmail.com>
2023-09-20 00:27:11 -0400

    finished backend and frontend(still needs styling)

---
+++ FastAPI/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+URL_DATABASE = 'postgresql://postgres:password@localhost:5433/FinanceTracker'
+
+engine = create_engine(URL_DATABASE)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
